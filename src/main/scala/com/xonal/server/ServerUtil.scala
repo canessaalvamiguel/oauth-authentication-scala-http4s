a708e5d548e9b39673bf882d5dc620d363d4404f
Miguel Canessa <canessaalvamiguel@gmail.com>
2023-09-26 18:35:39 -0500

    The application server

---
+++ src/main/scala/com/xonal/server/ServerUtil.scala
@@ -0,0 +1,21 @@
+package com.xonal.server
+
+import cats.effect.Async
+import com.xonal.config.Config
+import org.http4s.HttpRoutes
+import cats.effect.Resource
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.server.Server
+
+object ServerUtil {
+  def oauthServer[F[_]: Async](
+                                config: Config,
+                                service: HttpRoutes[F]
+                              ): Resource[F, Server] =
+    EmberServerBuilder
+      .default[F]
+      .withHost(config.server.hostValue)
+      .withPort(config.server.portValue)
+      .withHttpApp(service.orNotFound)
+      .build
+}
